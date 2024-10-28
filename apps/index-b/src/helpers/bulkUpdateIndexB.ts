bcf07957a479bfa8d4188a3e607c9ef11c4ccaea
Chris Drifte <chrisdrifte@gmail.com>
2024-10-28 08:34:46 +0100

    clean up bulk import function

--- apps/index-b/src/helpers/bulkUpdateIndexB.ts
+++ apps/index-b/src/helpers/bulkUpdateIndexB.ts
@@ -3,20 +3,17 @@ import { createFsKv } from '@product-backend/clients/fsKv';
 import { productDataToIndexBData } from './productDataToIndexBData';
 
 export async function bulkUpdateIndexB(productDataList: ProductData[]) {
-  console.info(
-    '[Index B] Indexed',
-    productDataList.map(productDataToIndexBData)
-  );
+  const indexBDataList = productDataList.map(productDataToIndexBData);
 
-  for (const productData of productDataList) {
-    const indexAData = productDataToIndexBData(productData);
+  console.info('[Index B] Indexed', indexBDataList);
 
+  for (const indexBData of indexBDataList) {
     // mimic storing in an index
     const fsKv = createFsKv();
     const kv = await fsKv.connect({
       url: 'INDEX_B',
     });
 
-    await kv.set(`PRODUCT_${indexAData.id}`, JSON.stringify(indexAData));
+    await kv.set(`PRODUCT_${indexBData.id}`, JSON.stringify(indexBData));
   }
 }
